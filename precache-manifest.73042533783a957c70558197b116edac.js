575e2fa37b48bfa4af43e772bd62a41fbd53647d
ramcharan-tech <kvsdcharan18@gmail.com>
2020-09-10 11:08:34 +0530

    gh-pages

---
+++ precache-manifest.73042533783a957c70558197b116edac.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc36342c8b9701292025",
+    "url": "/shopping2/css/about.0103099d.css"
+  },
+  {
+    "revision": "dc36342c8b9701292025",
+    "url": "/shopping2/js/about.50311efa.js"
+  },
+  {
+    "revision": "26c447d25e945770651f",
+    "url": "/shopping2/css/app.6aa0756c.css"
+  },
+  {
+    "revision": "26c447d25e945770651f",
+    "url": "/shopping2/js/app.04840a8d.js"
+  },
+  {
+    "revision": "0e23fd6eb4065eb7e58f",
+    "url": "/shopping2/css/chunk-383a92c9.62e41b49.css"
+  },
+  {
+    "revision": "0e23fd6eb4065eb7e58f",
+    "url": "/shopping2/js/chunk-383a92c9.68040591.js"
+  },
+  {
+    "revision": "0be5c7c59a58352dafc7",
+    "url": "/shopping2/css/chunk-3b05bc68.ae09e5fd.css"
+  },
+  {
+    "revision": "0be5c7c59a58352dafc7",
+    "url": "/shopping2/js/chunk-3b05bc68.3efb1aad.js"
+  },
+  {
+    "revision": "ade93bedf24bcd1de3a8",
+    "url": "/shopping2/css/chunk-3b496cd6.a7d30743.css"
+  },
+  {
+    "revision": "ade93bedf24bcd1de3a8",
+    "url": "/shopping2/js/chunk-3b496cd6.6379ab55.js"
+  },
+  {
+    "revision": "6698e8c17988c3643938",
+    "url": "/shopping2/css/chunk-ac180358.a99e4a23.css"
+  },
+  {
+    "revision": "6698e8c17988c3643938",
+    "url": "/shopping2/js/chunk-ac180358.8c90be4e.js"
+  },
+  {
+    "revision": "8965d00224c1bdb59113",
+    "url": "/shopping2/css/chunk-c5aa8b42.8952f3c7.css"
+  },
+  {
+    "revision": "8965d00224c1bdb59113",
+    "url": "/shopping2/js/chunk-c5aa8b42.21b42ef3.js"
+  },
+  {
+    "revision": "7f461baf04d2a3135771",
+    "url": "/shopping2/css/chunk-vendors.1d608a4b.css"
+  },
+  {
+    "revision": "7f461baf04d2a3135771",
+    "url": "/shopping2/js/chunk-vendors.869915a9.js"
+  },
+  {
+    "revision": "b79e577f6865ae737e7c83cf56d994d4",
+    "url": "/shopping2/img/login-img.b79e577f.png"
+  },
+  {
+    "revision": "0de836034aca9796dc5f0251ae53d9ed",
+    "url": "/shopping2/img/create-account.0de83603.svg"
+  },
+  {
+    "revision": "06017326ac3feb6477ca5d1e40489e0b",
+    "url": "/shopping2/img/logo.06017326.png"
+  },
+  {
+    "revision": "eabafcb5fcd2f91071976ba8a0a05f59",
+    "url": "/shopping2/index.html"
+  },
+  {
+    "revision": "06017326ac3feb6477ca5d1e40489e0b",
+    "url": "/shopping2/img/Vue-Icons.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/shopping2/robots.txt"
+  }
+];
